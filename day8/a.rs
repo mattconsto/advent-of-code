a88cf07a261a9891b28613a06a9f4560a3266889
Matthew Consterdine <mrconsterdine@googlemail.com>
2018-12-18 09:32:08 +0000

    Simplify

--- day8/a.rs
+++ day8/a.rs
@@ -1,34 +1,31 @@
 use std::io::prelude::*;
 use std::fs::File;
-use std::sync::{Arc, Mutex};
 
-static HEADER_LEN: usize = 2;
-
-fn visitor(data: &Vec<usize>, ptr: usize, counter: &Arc<Mutex<usize>>) -> usize {
-	// Recursively find the length of the children
-	let mut total = 0;
+fn visitor(data: &Vec<usize>, ptr: usize) -> (usize, usize) {
+	let (mut len, mut count) = (2, 0);
+	
+	// Recursively find children
 	for _i in 0..data[ptr + 0] {
-		total += visitor(data, ptr + HEADER_LEN + total, counter);
+		let (l, c) = visitor(data, ptr + len);
+		len += l;
+		count += c;
 	}
 
 	// Count up
-	let mut counter_deref = counter.lock().unwrap();
-	for i in 0..data[ptr + 1] {
-		*counter_deref += data[ptr + HEADER_LEN + total + i];
-	}
+	for i in 0..data[ptr + 1] {count += data[ptr + len + i];}
 
-	return HEADER_LEN + total + data[ptr + 1];
+	return (len + data[ptr + 1], count);
 }
 
 fn main() {
 	// Load the file, split by whitespace, and cast to usize
-	let mut file  = File::open("input.txt").expect("Unable to open file");
+	let mut file  = File::open("input.txt").expect("Cannot open file");
 	let mut input = String::new();
-	file.read_to_string(&mut input).expect("Unable to read file");
+	file.read_to_string(&mut input).expect("Cannot read file");
 	let numbers = input.split_whitespace().map(|s| s.parse().unwrap()).collect();
 
-	// Count up 
-	let counter = Arc::new(Mutex::new(0));
-	eprintln!("Length: {}", visitor(&numbers, 0, &counter));
-	println!("{}", counter.lock().unwrap());
+	// Display
+	let (len, count) = visitor(&numbers, 0);
+	eprintln!("Length: {}", len);
+	println!("{}", count);
 }
--- day8/b.rs
+++ day8/b.rs
@@ -1,49 +1,38 @@
 use std::io::prelude::*;
 use std::fs::File;
-use std::sync::{Arc, Mutex};
 
-static HEADER_LEN: usize = 2;
+fn visitor(data: &Vec<usize>, ptr: usize) -> (usize, usize) {
+	let (mut len, mut count, mut counts) = (2, 0, Vec::new());
 
-// Count the occurrences of a needle in a haystack
-fn occurences<T: PartialEq>(needle: T, haystack: &[T]) -> usize {
-	return haystack.iter().fold(0, |a,e| if *e == needle {a + 1} else {a});
-}
-
-fn visitor(data: &Vec<usize>, ptr: usize, counter: &Arc<Mutex<usize>>, multiplier: usize) -> usize {
-	// Recursively find the length of the children
-	let mut totals = Vec::new();
-	let mut total = 0;
+	// Recursively find children
 	for _i in 0..data[ptr + 0] {
-		totals.push(total);
-		total += visitor(data, ptr + HEADER_LEN + total, counter, 0);
+		let (l, c) = visitor(data, ptr + len);
+		len += l;
+		counts.push(c);
 	}
 
 	// Count up
-	if multiplier > 0 {
-		if data[ptr + 0] > 0 {// Recurse
-			for i in 0..data[ptr + 0] {
-				let offset = ptr + HEADER_LEN + total;
-				let occur = occurences(i + 1, &data[offset..(offset + data[ptr + 1])]);
-				if occur > 0 {visitor(data, ptr + HEADER_LEN + totals[i], counter, multiplier * occur);}
-			}
-		} else {// Or add up
-			let mut deref = counter.lock().unwrap();
-			for i in 0..data[ptr + 1] {*deref += data[ptr + HEADER_LEN + total + i] * multiplier;}
+	if data[ptr + 0] > 0 {
+		for i in 0..data[ptr + 1] {
+			let p = data[ptr + len + i];
+			if p > 0 && p <= counts.len() {count += counts[p - 1];}
 		}
+	} else {
+		for i in 0..data[ptr + 1] {count += data[ptr + len + i];}
 	}
 
-	return HEADER_LEN + total + data[ptr + 1];
+	return (len + data[ptr + 1], count);
 }
 
 fn main() {
 	// Load the file, split by whitespace, and cast to usize
-	let mut file  = File::open("input.txt").expect("Unable to open file");
+	let mut file  = File::open("input.txt").expect("Cannot open file");
 	let mut input = String::new();
-	file.read_to_string(&mut input).expect("Unable to read file");
+	file.read_to_string(&mut input).expect("Cannot read file");
 	let numbers = input.split_whitespace().map(|s| s.parse().unwrap()).collect();
 
-	// Count up 
-	let counter = Arc::new(Mutex::new(0));
-	eprintln!("Length: {}", visitor(&numbers, 0, &counter, 1));
-	println!("{}", counter.lock().unwrap());
+	// Display
+	let (len, count) = visitor(&numbers, 0);
+	eprintln!("Length: {}", len);
+	println!("{}", count);
 }
